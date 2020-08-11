5dce90c5e68576ffc8bc4df9bffe68c3e16dca95
angelhpascual <angelhpascual@gmail.com>
2020-08-11 14:45:34 +0200

    useState. Añadir productos al carrito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Producto from "./components/Producto";
 
 function App() {
+  //Crear listado de productos
+
+  const [productos, setProductos] = useState([
+    { id: 1, nombre: "Camisa ReactJs", precio: 50 },
+    { id: 2, nombre: "Camisa Vuejs", precio: 40 },
+    { id: 3, nombre: "Camisa Nodejs", precio: 30 },
+    { id: 4, nombre: "Camisa Angular", precio: 20 },
+  ]);
+
+  //Variable para la fecha y mandarla por props a Footer
   const fecha = new Date().getFullYear();
 
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <Fragment>
       <Header titulo="Tienda Virtual" />
+
+      <h1>Lista de Productos</h1>
+
+      {productos.map((producto) => (
+        <Producto
+        
+        key= {producto.id}
+        producto={producto} />
+
+      ))}
+
       <Footer fecha={fecha} />
-    </div>
+    </Fragment>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 function Header({ titulo }) {
-  console.log({ titulo });
-
   return <h1 className="encabezado">{titulo}</h1>;
 }
 
